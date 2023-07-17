0cd236969ed0276c2dbc533fa5bcb5b22b8f2a03
RenanEfrem <renan.efrem57@gmail.com>
2023-07-17 13:01:02 -0300

    🔥Alterações AlertUI/Responsividade/Design

--- js/form.js
+++ js/form.js
@@ -3,11 +3,13 @@ let password = document.getElementById('password').value;
 
 let alertModal = document.getElementById('alert-modal');
 
+let alertModalRegister = document.getElementById('alert-modal-register');
+
 const form = document.getElementById('login-form')
 form.addEventListener('submit', e => {
     e.preventDefault()
 })
-function validateForm() {
+function validateFormLogin() {
     alertModal.style.opacity = 1;
     alertModal.style.visibility = 'visible';
     setTimeout(() => {
@@ -15,3 +17,16 @@ function validateForm() {
         alertModal.style.visibility = 'hidden';
     }, 2000)
 }
+function validateFormRegister() {
+    alertModalRegister.style.visibility = 'visible';
+    alertModalRegister.style.opacity = 1;
+    setTimeout(() => {
+        alertModalRegister.style.opacity = 0;
+        alertModalRegister.style.visibility = 'hidden';
+    }, 2000)
+}
+
+const formRegister = document.getElementById('register-form')
+formRegister.addEventListener('submit', e => {
+    e.preventDefault()
+})
